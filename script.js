f5871a7bd776bb48509278e2a322c0902ee6045d
Maria <masha_200714@mail.ru>
2025-09-21 13:20:23 +0300

    added an object personalMovieDB

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 "use strict"
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
+const personalMovieDB ={
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
