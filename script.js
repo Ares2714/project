0e29ce92945c33d88e74ffe99b4372a44c8dffc6
Maria <masha_200714@mail.ru>
2025-09-21 13:29:55 +0300

    added a questions about movies

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 "use strict"
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
+
 const personalMovieDB ={
     count: numberOfFilms,
     movies: {},
@@ -7,3 +8,11 @@ const personalMovieDB ={
     genres: [],
     privat: false
 };
+
+const film = prompt('Один из последних просмотренных фильмов?',''),
+    rate = prompt('На сколько оцените его?',''),
+    film2 = prompt('Один из последних просмотренных фильмов?',''),
+    rate2 = prompt('На сколько оцените его?','');
+
+personalMovieDB.movies[film] = rate;
+personalMovieDB.movies[film2] = rate2;
